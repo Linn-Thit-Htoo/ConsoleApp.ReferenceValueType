2714160b0fa297931add1965b0c61af97626f297
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-13 19:24:09 +0630

    Add project files.

---
+++ ConsoleApp.ReferenceValueType/Program.cs
@@ -0,0 +1,33 @@
+﻿public class ValueTypeExample
+{
+    public static void Main(string[] args)
+    {
+        #region Value Type
+
+        //int value = 19;
+        //Console.WriteLine($"Original Value: {value}");
+        //SetValue(value);
+        //Console.WriteLine($"After modifying: {value}");
+
+        #endregion
+
+        #region Reference Type
+
+        int value = 19;
+        Console.WriteLine($"Original Value: {value}");
+        SetValueRef(ref value);
+        Console.WriteLine($"After modifying: {value}");
+
+        #endregion
+    }
+
+    public static void SetValue(int value)
+    {
+        value += 1;
+    }
+
+    public static void SetValueRef(ref int value)
+    {
+        value += 1;
+    }
+}
